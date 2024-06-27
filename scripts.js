e2af922e74dbb32b7be02260048fcb87c45a01aa
Abeos <abeobright123@gmail.com>
2024-06-27 15:07:40 +0000

    My first commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function message(){
+    alert("welcome to my website.!!!!");
+}
+
+function something(){
+    let person = prompt("what is your name: ");
+    alert("Nice to meet you, " + person)
+}
